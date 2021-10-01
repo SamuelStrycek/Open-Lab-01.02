d0b5e9f1107f049ddd318c651de4c6d600f3b6c0
SamuelStrycek <91180378+SamuelStrycek@users.noreply.github.com>
2021-10-01 09:33:26 +0200

    Add files via upload

---
+++ Open-Lab-01.02/Open-Lab-01.02/Exercise.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_01._02
+{
+    public class Exercise
+    {
+        public int Sum(int a, int b)
+        {   
+            return (a + b)
+        }
+    }
+}
